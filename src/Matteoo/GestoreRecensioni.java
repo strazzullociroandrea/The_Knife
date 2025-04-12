092780ac6ebfc24f40c0c88bdb19077087cc72c4
mmongelli5 <mmongelli@studenti.uninsubria.it>
2025-04-12 11:39:47 +0200

    Aggiunta classe temporanea per avere una lista di Recensioni utile alla classe Utente e alle sue sottoclassi

---
+++ src/Matteoo/GestoreRecensioni.java
@@ -0,0 +1,27 @@
+package src.Matteoo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestoreRecensioni {
+    //attributi
+    private static List<Recensione> recensioni = new ArrayList<>();
+    //metodi
+    public static void aggiungiRecensione(Recensione r) {
+        recensioni.add(r);
+    }
+
+    public static List<Recensione> getRecensioni() {
+        return recensioni;
+    }
+
+    public static double mediaStelle() {
+        if (recensioni.isEmpty()) return 0;
+        int somma = 0;
+        for (Recensione r : recensioni) {
+            somma += r.getStelle();
+        }
+        return (double) somma / recensioni.size();
+    }
+}
+
