704d31ea61dea9f862e0196744790f69c8bd4fff
rgrubini <rgrubini@studenti.uninsubria.it>
2025-04-17 12:52:45 +0200

    creazione attributi, costruttori, metodi (completo, da verificare dopo la conclusione della classe ristorante) classi utente, cliente, ristoratore e password

--- src/Riccardo/Ristoratore.java
+++ src/Riccardo/Ristoratore.java
@@ -22,25 +22,47 @@ public class Ristoratore extends Utente {
     }
 
     //metodi
-    //metodo per creare ristorante
-
-    public void creaRistorante(String nome, String nazione, String citta, String indirizzo, String tipoCucina,
-                               boolean delivery, boolean prenotazioneOnline, double minPrezzo, double maxPrezzo) {
-        this.ristorante = new Ristorante (nome, nazione, citta, indirizzo,tipoCucina,delivery, prenotazioneOnline, minPrezzo,maxPrezzo);
+    /**
+     *
+     * metodo per creare un ristorante da ristoratore (lo aggiunge in automatico alla lista dei gestiti)
+     * @param nome
+     * @param nazione
+     * @param citta
+     * @param indirizzo
+     * @param lat
+     * @param log
+     * @param min
+     * @param max
+     * @param delivery
+     * @param prenotazione
+     * @param tipoCucina
+     */
+    public void creaRistorante(String nome, String nazione, String citta, String indirizzo, double lat, double log, double min, double max, boolean delivery,  boolean prenotazione, String tipoCucina) {
+        this.ristorante = new Ristorante (nome, nazione, citta, indirizzo,lat, log,min, max, delivery,prenotazione,tipoCucina);
         ristorantiGestiti.add(ristorante);
     }
 
-    //metodo per aggiungere ristorante
+    /**
+     * metodo per aggiungere ristorante
+     * @param ristorante
+     */
     public void aggiungiRistorante(Ristorante ristorante) {
         ristorantiGestiti.add(ristorante);
     }
 
-    //metodo per prendere lista ristoranti gestiti
+    /**
+     * metodo per prendere lista ristoranti gestiti
+     * @return
+     */
     public List<Ristorante> getRistorantiGestiti() {
         return ristorantiGestiti;
     }
 
-    //metodo per prendere lista visualizzare le recensioni
+    /**
+     *
+     * @param ristorante
+     */
+
     public void visualizzaRecensioni(Ristorante ristorante) {
         if (ristorantiGestiti.contains(ristorante)) {
             for (Recensione r : ristorante.getRecensioni()) {
@@ -51,7 +73,11 @@ public class Ristoratore extends Utente {
         }
     }
 
-    //metodo per rispondere alle recensioni
+    /**
+     * metodo per rispondere alle recensioni
+     * @param recensione
+     * @param risposta
+     */
     public void rispondiRecensione(Recensione recensione, String risposta) {
         if (recensione.getRisposta() == null) {
             recensione.setRisposta(risposta);
@@ -61,7 +87,10 @@ public class Ristoratore extends Utente {
 
     }
 
-    //metodo per visualizzare media stelle e numero delle recensioni
+    /**
+     * metodo per visualizzare media stelle e numero delle recensioni
+     * @param ristorante
+     */
     public static void visualizzaRiepilogo(GestoreRecensioni ristorante) {
         System.out.println("media delle stelle: "+ristorante.mediaStelle());
         System.out.println("numero recensioni "+ristorante.numeroRecensioni());
