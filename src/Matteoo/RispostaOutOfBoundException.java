2308534d5887557eed4620256e7aa3436634cc0a
mmongelli5 <mmongelli@studenti.uninsubria.it>
2025-04-29 11:14:55 +0200

    aggiunta descrizione dei metodi e costruttori della documentazione delle classi

--- src/Matteoo/RispostaOutOfBoundException.java
+++ src/Matteoo/RispostaOutOfBoundException.java
@@ -1,6 +1,9 @@
 package src.Matteoo;
 
 public class RispostaOutOfBoundException extends RuntimeException {
+    /**
+     * costruttore per creare un'eccezione personalizzata lanciata nel caso l'attributo risposta superi i 250 caratteri o sia vuota
+     */
     public RispostaOutOfBoundException() {
         super("La stringa è più lunga di 250 caratteri o è vuota");
     }
