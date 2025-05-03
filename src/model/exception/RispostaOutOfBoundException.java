1ded1081fc5726d6070c44bee2ce931de6784d9d
mmongelli5 <mmongelli@studenti.uninsubria.it>
2025-05-03 14:56:26 +0200

    aggiunti i commenti degli attributi della classe Recensione e delle classi: Recensione, RispostaOutOfBoundException, StelleOutOfBoundException, RecensioneOutOfBoundException.

--- src/model/exception/RispostaOutOfBoundException.java
+++ src/model/exception/RispostaOutOfBoundException.java
@@ -1,5 +1,14 @@
 package src.model.exception;
 
+/**
+ * Classe eccezione personalizzata realizzata per gestire i casi in cui la risposta testuale associata alla recensione sia vuota o superi i 250 caratteri
+ * @version 1.0
+ *  * @Author Strazzullo Ciro Andrea
+ *  * @Author Riccardo Giovanni Rubini
+ *  * @Author Matteo Mongelli
+ *  * @Author Nicolò Valter Girardello
+ */
+
 public class RispostaOutOfBoundException extends RuntimeException {
     /**
      * costruttore per creare un'eccezione personalizzata lanciata nel caso l'attributo risposta superi i 250 caratteri o sia vuota
