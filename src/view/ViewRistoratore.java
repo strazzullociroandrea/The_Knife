880a5d2f3271d1f09488decc3366267379e7b38b
rgrubini <rgrubini@studenti.uninsubria.it>
2025-05-12 10:35:56 +0200

    inizio della view ristoratore

--- src/view/ViewRistoratore.java
+++ src/view/ViewRistoratore.java
@@ -1,4 +1,20 @@
 package src.view;
 
+import java.util.Scanner;
+
 public class ViewRistoratore {
+    public static void view() throws Exception {
+        try (Scanner s = new Scanner(System.in)) {
+            int scelta = convertiScannerIntero( """
+                    \n\n
+                    Menù ristoratore:
+                    1. Crea ristorante
+                    2. Lista ristoranti gestiti (modifica, elimina, aggiungi ristorante ai gestiti)
+                    3. Gestisci recensioni ristoranti gestiti (visualizza recensioni, rispondi a recensioni)
+                """, s);
+            switch ( scelta) {
+                case 1:
+            }
+        }
+    }
 }
