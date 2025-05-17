41b250e32f7c678cf98e8ff70f436d7ecfa12991
mmongelli5 <mmongelli@studenti.uninsubria.it>
2025-05-17 22:29:57 +0200

    ViewCliente quasi completata

--- src/view/ViewCliente.java
+++ src/view/ViewCliente.java
@@ -1,10 +1,18 @@
 package src.view;
 
 import src.dao.GestoreFile;
+import src.model.Cliente;
+import src.model.Recensione;
 import src.model.Ristorante;
+import src.model.Utente;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+
+import static src.dao.GestoreFile.caricaUtenti;
+
 
 /**
  * Classe ViewCliente che rappresenta l'interfaccia grafica presentata al cliente
@@ -13,6 +21,7 @@ import java.util.Scanner;
  * * @Author Riccardo Giovanni Rubini
  * * @Author Matteo Mongelli
  */
+
 
 public class ViewCliente {
     /**
@@ -26,87 +35,202 @@ public class ViewCliente {
 
     //metodi
 
+    /*
+        public void prenotaTavolo(String ristorante) {
+            try {
+                for (Ristorante r : GestoreFile.caricaRistoranti(PATHRISTORANTI)) {
+                    if (ristorante.equals(r.getNome())) {
+                        r.;
+                    }
+                }
+
+
+            }
+        } catch(
+        IOException e;)
+
+        {
+            throw new RuntimeException(e);
+        }
+    */
+
+    private static String gestisciInput(String msg, Scanner scanner) {
+        String input = "";
+        do {
+            System.out.println(msg);
+            input = scanner.nextLine();
+        } while (input.isEmpty());
+        return input;
+    }
+
 
     /**
-     * metodo per accedere alla view del cliente
+     * metodo per visualizzare, interagire e scorrere dinamicamente i ristoranti
      *
-     * @throws Exception
+     * @param u               l'utente che interagisce col ristorante e che può lasciare una recensione
+     * @param s               lo scanner che permette all'utente di lasciare una recensione se lo richiede (stelle e testo)
+     * @param listaRistoranti la lista di ristoranti che si vuole scorrere
      */
-    public static void view() throws Exception {
+
+    private static void navigazioneRistoranti(Cliente u, Scanner s, List<Ristorante> listaRistoranti) {
+        if (listaRistoranti == null || listaRistoranti.isEmpty()) {
+            System.out.println("Nessun ristorante trovato.");
+            return;
+        }
+
+        int indice = 0;
+        boolean visualizza = true;
+
+        while (visualizza) {
+            Ristorante ristoranteCorrente = listaRistoranti.get(indice);
+            System.out.println("\n--- Ristorante " + (indice + 1) + " di " + listaRistoranti.size() + " ---");
+            System.out.println(ristoranteCorrente.visualizzaRistorante());
+
+            System.out.println("""
+                        Scegli un'opzione:
+                        1. Lascia una recensione
+                        2. Vai al ristorante successivo
+                        3. Torna al ristorante precedente
+                        4. Inserisci il ristorante tra i preferiti
+                        5. Modifica la recensione lasciata al ristorante 
+                        6. Esci dalla visualizzazione
+                    """);
+
+            int sceltaInterna = ViewBase.convertiScannerIntero("Scelta:", s);
+
+            switch (sceltaInterna) {
+                case 1:
+                    System.out.println("Scrivi la tua recensione:");
+                    String descrizione = s.nextLine();
+                    int stelle = ViewBase.convertiScannerIntero("Inserisci il numero di stelle (0-5):", s);
+                    u.aggiungiRecensione(ristoranteCorrente, stelle, descrizione);
+                    System.out.println("Recensione aggiunta con successo!");
+                    break;
+
+                case 2:
+                    if (indice < listaRistoranti.size() - 1) {
+                        indice++;
+                    } else {
+                        System.out.println("Hai raggiunto l'ultimo ristorante.");
+                    }
+                    break;
+
+                case 3:
+                    if (indice > 0) {
+                        indice--;
+                    } else {
+                        System.out.println("Sei già al primo ristorante.");
+                    }
+                    break;
+
+                case 4:
+                    u.aggiungiPreferito(ristoranteCorrente);
+
+                case 5:
+                    for (Recensione r : ristoranteCorrente.getRecensioni())
+                        if (r.getId().)
+                            if (u.getRecensioniMesse().contains(ristoranteCorrente.)) {
+                            }
+                    u.modificaRecensione()
+
+                case 6:
+                    visualizza = false;
+                    break;
+
+                default:
+                    System.out.println("Scelta non valida.");
+            }
+        }
+    }
+
+
+    /**
+     * metodo per acccedere alla view del cliente
+     *
+     * @param u l'utente di tipo Cliente che interagisce con la view
+     */
+
+    public static void view(Cliente u) {
         try (Scanner s = new Scanner(System.in)) {
+
 
             System.out.println("\n--- Menu Cliente ---");
             System.out.println("1. Visualizza tutti i ristoranti");
-            System.out.println("2. Filtra ristoranti per parametri");
-            System.out.println("3. Prenota un tavolo");
-            System.out.println("4. Gestisci prenotazioni");
-            System.out.println("5. Scrivi una recensione");
-            System.out.println("6. Visualizza/modifica dati personali");
-            System.out.println("7. Logout");
+            System.out.println("2. Cerca ristoranti filtrando per parametri");
+            System.out.println("3. Scrivi una recensione");
+            System.out.println("4. Visualizza/modifica dati personali");
+            System.out.println("5. Logout");
 
 
             int scelta = ViewBase.convertiScannerIntero("Scegli un'opzione:", s);
 
             switch (scelta) {
                 case 1:
-                    try {
-                        for (Ristorante r : GestoreFile.caricaRistoranti(PATHRISTORANTI)) {
-                            System.out.println(r);
-                        }
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
+                    List<Ristorante> listaRistoranti = GestoreFile.caricaRistoranti(PATHRISTORANTI);
+                    if (listaRistoranti.isEmpty()) {
+                        System.out.println("Nessun ristorante trovato.");
+                        break;
                     }
-
+                    navigazioneRistoranti(u, s, listaRistoranti);
                     break;
+
 
                 case 2:
                     System.out.println("\n--- Scegliere i criteri del filtro ---");
-                    System.out.println("1. mostra solo disponibili");
-                    System.out.println("2. mostra solo ristoranti che fanno delivery");
-                    System.out.println("3. ricerca combinata");
-                    System.out.println("4. ");
-                    System.out.println("5. ");
-                    System.out.println("6. ");
-                    System.out.println("7. ");
 
-                    int sceltaFiltro = ViewBase.convertiScannerIntero("Scegli un'opzione:", s);
-                    switch (sceltaFiltro) {
-                        case 1:
-                            System.out.println("inserire la città dove effettuare la ricerca");
-                            try (Scanner sc = new Scanner(System.in)) {
-                                String location = sc.nextLine();
-                                Ristorante.perPrenotazioneOnline(GestoreFile.caricaRistoranti(PATHRISTORANTI), true, location);
-                            } catch (Exception e) {
-                                System.out.println("Cità non trovata");
-                            }
-                            break;
-
-                        case 2:
-                            Ristorante.perDelivery();
-                            break;
-
-
-                        case 3:
-                            try(Scanner sc = new Scanner(System.in)) {
-                                System.out.println("inserire una location");
-                                String location = sc.nextLine();
-                                System.out.println("inserire il tipo di cucina desiderata tra: /n" + Ristorante.);
+                    try (Scanner sc = new Scanner(System.in)) {
+                        System.out.println("inserire una location");
+                        String location = sc.nextLine();
+                        System.out.println("inserire il tipo di cucina desiderata tra: /n");
+                        String tipoCucina = sc.nextLine();
+                        System.out.println("inserire il prezzo minimo richiesto");
+                        double prezzoMinimo = sc.nextDouble();
+                        System.out.println("inserire il prezzo massimo richiesto");
+                        double prezzoMassimo = sc.nextDouble();
+                        System.out.println("Digitare 1 per ricercare solo ristoranti con delivery, altrimenti premere invio");
+                        boolean delivery = false;
+                        String deliveryText = sc.nextLine();
+                        if (deliveryText.equals("1")) {
+                            delivery = true;
+                        }
+                        System.out.println("Digitare 1 per ricercare solo ristoranti con prenotazione disponibile, altrimenti premere invio");
+                        boolean prenotazione = false;
+                        String prenotazioneText = sc.nextLine();
+                        if (deliveryText.equals("1")) {
+                            prenotazione = true;
+                        }
+                        System.out.println("inserire il minimo di stelle richiesto");
+                        int stelle = ViewBase.convertiScannerIntero(sc.nextLine(), s);
 
 
-                                Ristorante.combinata();
-                            }
+                        List<Ristorante> filtrati = Ristorante.combinata(GestoreFile.caricaRistoranti(PATHRISTORANTI), location, tipoCucina, prezzoMinimo, prezzoMassimo, true, delivery, true, prenotazione, stelle);
 
-
+                        navigazioneRistoranti(u, s, filtrati);
 
                     }
 
 
                 case 3:
-                    prenotaTavolo(cliente, scanner, ristoranti);
+                    try (Scanner sc = new Scanner(System.in)) {
+                        System.out.println("inserire il nome del ristorante da recensire");
+                        String nome = sc.nextLine();
+                        for (Ristorante r : GestoreFile.caricaRistoranti(PATHUTENTI)) {
+                            if (nome.equals(r.getNome())) {
+                                System.out.println("scrivere la recensione");
+                                String descrizione = sc.nextLine();
+                                System.out.println("inserire il numero di stelle");
+                                int stelle = ViewBase.convertiScannerIntero(sc.nextLine(), s);
+                                u.aggiungiRecensione(r, stelle, descrizione);
+                            }
+
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
 
                 case 4:
-                    gestisciPrenotazioni(cliente, scanner);
+
                     break;
 
                 case 5:
@@ -119,9 +243,8 @@ public class ViewCliente {
 
             }
 
-        } catch (
-                Exception e) {
-            throw new Exception();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
 
     }
