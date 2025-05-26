15cc8c68eb7c436cf38870edd389112ad8afd5f8
rgrubini <rgrubini@studenti.uninsubria.it>
2025-05-26 09:54:15 +0200

    modifiche ristoratore e view ristorante

--- src/model/exception/InvalidNomeException.java
+++ src/model/exception/InvalidNomeException.java
@@ -1,5 +1,12 @@
 package src.model.exception;
 
+/**
+ * classe eccezione che gestisce i casi in cui nome e cognome siano stringhe vuote
+ * @version 1.0
+ * @Author Strazzullo Ciro Andrea
+ * @Author Riccardo Giovanni Rubini
+ * @Author Matteo Mongelli
+ */
 public class InvalidNomeException extends RuntimeException {
     public InvalidNomeException() {
         super("il nome e il cognome non possono essere stringhe vuote");
