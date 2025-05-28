1656fe65ce8ed31b27e4e7077f5ed882e4118506
strazzullociroandrea <ciroandreastrazzullo@icloud.com>
2025-05-28 11:18:37 +0200

    Creazione del file se non esiste

--- src/dao/GestoreFile.java
+++ src/dao/GestoreFile.java
@@ -23,12 +23,29 @@ public class GestoreFile {
     }
 
     /**
+     * Metodo per creare un file con contenuto iniziale se non esiste
+     * @param path path del file da creare
+     * @param contenutoIniziale contenuto iniziale del file
+     * @throws IOException in caso di errore durante la creazione del file
+     */
+    private static void creaFile(String path, String contenutoIniziale) throws IOException {
+        File file = new File(path);
+        if (!file.exists()) {
+            file.getParentFile().mkdirs(); // crea le cartelle che contengono il file se non esistono
+            try (Writer writer = new FileWriter(file)) {
+                writer.write(contenutoIniziale);
+            }
+        }
+    }
+
+    /**
      * Metodo per salvare la lista di ristoranti in un file JSON
      * @param ristoranti lista di ristoranti da salvare
      * @param path path del file in cui salvare i ristoranti
      * @throws IOException in caso di errore durante la scrittura del file
      */
     public static void salvaRistoranti(List<Ristorante> ristoranti, String path) throws IOException {
+        creaFile(path, "[]"); // crea il file se non esiste con un array vuoto come contenuto iniziale
         try (Writer writer = new FileWriter(path)) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             gson.toJson(ristoranti, writer);
@@ -44,6 +61,7 @@ public class GestoreFile {
      * @throws IOException in caso di errore durante la lettura del file
      */
     public static List<Ristorante> caricaRistoranti(String path) throws IOException {
+        creaFile(path, "[]"); // crea il file se non esiste con un array vuoto come contenuto iniziale
         try (Reader reader = new FileReader(path)) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             return gson.fromJson(reader, new TypeToken<List<Ristorante>>() {}.getType());
@@ -59,6 +77,7 @@ public class GestoreFile {
      * @throws IOException in caso di errore durante la scrittura del file
      */
     public static void salvaUtenti(List<Utente> utenti, String path) throws IOException {
+        creaFile(path, "[]"); // crea il file se non esiste con un array vuoto come contenuto iniziale
         JsonArray array = new JsonArray();
         for (Utente u : utenti) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -91,8 +110,8 @@ public class GestoreFile {
      * @throws IOException in caso di errore durante la lettura del file
      */
     public static List<Utente> caricaUtenti(String path) throws IOException {
+        creaFile(path, "[]"); // crea il file se non esiste con un array vuoto come contenuto iniziale
         List<Utente> utenti = new ArrayList<>();
-
         try (Reader reader = new FileReader(path)) {
             JsonArray array = JsonParser.parseReader(reader).getAsJsonArray();
 
