e366bc8c6b3f9a26a373c57d0ca491c29b7e4c6e
mmongelli5 <mmongelli@studenti.uninsubria.it>
2025-06-01 17:26:44 +0200

    -miglioramento codice di controllo di Viewcliente e testing

--- src/view/ViewCliente.java
+++ src/view/ViewCliente.java
@@ -293,7 +293,7 @@ public class ViewCliente {
                             prenotazione = true;
                         }
 
-                        int stelleMin = ViewBase.convertiScannerIntero(gestisciInput("inserire il minimo di stelle richiesto", s, true), s);
+                        int stelleMin = ViewBase.convertiScannerIntero("inserire il minimo di stelle richiesto"2ù, s);
 
 
                         List<Ristorante> filtrati = Ristorante.combinata(GestoreFile.caricaRistoranti(PATHRISTORANTI), location, tipoCucina, prezzoMinimo, prezzoMassimo, true, delivery, true, prenotazione, stelleMin);
